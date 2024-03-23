61df8b66d82b1c6e6f3e969146df7280fd98a4d5
Manthan666 <manthanparekh455@gmail.com>
2024-03-24 00:18:19 +0530

    Database modify by manthan

--- fogot-password.php
+++ fogot-password.php
@@ -66,7 +66,7 @@
 </head>
 
 <body>
-    <form action="send-password-reset.php" method="post" autocomplete="off">
+    <form action="send-password-reset.php" method="get" autocomplete="off">
         <div class="container">
             <h3>Forgot Your Password?</h3>
             <label for="email">Enter Your Registered Email Id : </label>
