9ca8c369d690705cea5d7900c9335908bc0272eb
Manthan666 <manthanparekh455@gmail.com>
2024-03-29 22:51:39 +0530

    Database modify by manthan

--- login1.php
+++ login1.php
@@ -1,10 +1,19 @@
 <?php
-require_once 'config.php';
+$showError = false;
+// require_once 'config.php';
+if (isset($_POST['email'])) {
+    $server = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $db_name = "registered users";
 
+    $link = mysqli_connect($server, $username, $password, $db_name);
+
+    if ($link === false) {
+        die("ERROR : - connection to this database failed due to" . mysqli_connect_error());
+    }
     $email = $_POST['email'];
     $password = $_POST['password'];
-    // $showError = false;
-
     $stmt = mysqli_prepare($link, "SELECT * FROM `registration` WHERE email = ? AND `conf.password` = ?");
     mysqli_stmt_bind_param($stmt, "ss", $email, $password);
     mysqli_stmt_execute($stmt);
@@ -14,15 +23,79 @@ require_once 'config.php';
         header("Location: index.html");
         exit();
     } else {    //User not exist in data base or entered wrong password
-        function alert1($message) {
-            echo "<script>alert('$message');
-            window.location.href='login1.html'
-            </script>";
-        }
-        alert1("You are not registered yet or entered worng password!!");
-        // $showError = "You are not registered yet or entered wrong password";
+        // function alert1($message) {
+        //     echo "<script>alert('$message');
+        //     window.location.href='login1.html'
+        //     </script>";
+        // }
+        // alert1("You are not registered yet or entered worng password!!");
+        $showError = true;
     }
     
     mysqli_stmt_close($stmt);
     mysqli_close($link);
-?>
+}
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link rel="shortcut icon"
+        href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmUUSo7THCDrJG629AyXKTeQr1Cl-CpU6jgQ4WD63gmZYyrvU6SrKx17XxiIH7D8z7M_w&usqp=CAU">
+    <link rel="shortcut icon" href="signup favicon.png" type="image/x-icon">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="login1.css">
+</head>
+
+<body>
+    <?php
+    if($showError) {
+    echo '<h2>Error!!!You never registered on our website first go to sign up and register yourself or you may entered wrong password</h2>';
+    }
+    ?>
+    <div class="container1">
+        <ul class="navbar">
+            <div class="icon">
+                <a href="index.html"><i class='bx bxs-home'></i></a>
+            </div>
+            <div class="">
+                <a href=""></a>
+            </div>
+            <div class="">
+                <a href=""></a>
+            </div>
+            <div class="">
+                <a href=""></a>
+            </div>
+        </ul>
+    </div>
+    <div class="box">
+        <div class="wrapper">
+            <form action="login1.php" method="post">
+                <h1>Login</h1>
+                <div class="input-box">
+                    <input type="email" name="email" placeholder="Enter Your user email" required>
+                    <i class='bx bx-user'></i>
+                </div>
+                <div class="input-box">
+                    <input type="password" name="password" placeholder="Enter Your Password" required>
+                    <i class='bx bx-lock'></i>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox" name="remember" id="">Remember Me</label>
+                    <a href="#" target="_blank">Forgot Password</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="register-link">
+                    <p>Don't Have An Account?? <a href="signup1.html">Register</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
--- signup1.php
+++ signup1.php
@@ -1,70 +1,145 @@
 <?php
-require_once 'config.php';
+$showAlert = false;
+$showError = false;
+$showSuccess = false;
+
+// require_once 'config.php';
+if (isset($_POST['email'])) {
+    $server = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $db_name = "registered users";
+
+    $link = mysqli_connect($server, $username, $password, $db_name);
+
+    if ($link === false) {
+        die("ERROR : - connection to this database failed due to" . mysqli_connect_error());
+    }
 
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $confpassword = $_POST['cpassword'];
-    $username_error = null;
-    $email_error = null;
-    $password_error = null;
-    $cpassword_error = null;
+    $confpassword = $_POST['cpassword'];   
 
     if ($password !== $confpassword) {
-        function alert($message)
-        {
-            echo "<script>
-                alert('$message');
-                window.location.href= 'signup.html'
-            </script>";
-        }
-        alert("Failed to register because your password and confirm password not match");
-    } 
-
-    else if(empty(trim($name))){
-        echo $username_error = "Field user name is empty";
-    }
-    else if(empty(trim($email))){
-        echo $email_error = "Field email is empty";
-    }
-    else if(empty(trim($password))){
-        echo $password_error = "Field password is empty";
-    }
-    else if(empty(trim($email))){
-        echo $cpassword_error = "Field confirm password is empty";
-    }
-    else{
+        // function alert($message)
+        // {
+        //     echo "<script>
+        //         alert('$message');
+        //         window.location.href= 'signup.html'
+        //         </script>";
+        // }
+        // alert("Failed to register because your password and confirm password not match");
+        $showAlert = true;
+    } else{
         $query = mysqli_query($link, "SELECT * FROM `registration` WHERE email = '$email'");
         if (mysqli_num_rows($query) > 0) {
-            function alert($message)
-            {
-                echo "<script>alert('$message');
-            window.location.href='login.html'
-            </script>";
-            }
-            alert("Email id already in use sending you at login page!!");
-        } else {
-            $stmt = mysqli_prepare($link, "INSERT INTO `registration` (`name`, `email`, `password`, `conf.password`, `dt`) VALUES (?, ?, ?, ?, current_timestamp())");
+        //     function alert($message)
+        //     {
+        //         echo "<script>alert('$message');
+        //     window.location.href='login.html'
+        //     </script>";
+        // }
+        // alert("Email id already in use sending you at login page!!");
+        $showError = true;
+    } else {
+        $stmt = mysqli_prepare($link, "INSERT INTO `registration` (`name`, `email`, `password`, `conf.password`, `dt`) VALUES (?, ?, ?, ?, current_timestamp())");
 
-            // Bind parameters to the prepared statement
-            mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $password, $confpassword);
-
-            // Execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                function alert($message)
-                {
-                    echo "<script>alert('$message');
-                window.location.href='index.html'
-                </script>";
-                }
-                alert("You are registered successfully , Thank you for registration");
+        // Bind parameters to the prepared statement
+        mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $password, $confpassword);
+        
+        // Execute the prepared statement
+        if (mysqli_stmt_execute($stmt)) {
+            // function alert($message)
+            //     {
+            //         echo "<script>alert('$message');
+            //         window.location.href='index.html'
+            //         </script>";
+            //     }
+            //     alert("You are registered successfully , Thank you for registration");
+            $showSuccess = true;
             } else {
-                echo "ERROR: Unable to execute $sql. " . mysqli_error($link);
+                echo "Facing some technical issue sorry for this inconvinience caused!!";
             }
-
+            
             // Close the prepared statement
             mysqli_stmt_close($stmt);
         }
     }
     $link->close();
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="shortcut icon"
+        href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmUUSo7THCDrJG629AyXKTeQr1Cl-CpU6jgQ4WD63gmZYyrvU6SrKx17XxiIH7D8z7M_w&usqp=CAU">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="signup1.css">
+</head>
+
+<body>
+<?php
+    if($showAlert) {
+    echo '<h2 id="alert">Alert!!!Password And Confirm Password Do Not Match</h2>';
+    }
+    else if($showError) {
+        echo '<h2 id="error">Error!!!This Email Id Already In use You Should Go To Login Page</h2>';
+    }
+    else if($showSuccess){
+        echo '<h2 id="Success">Successfull!!!Your Registration Successfully Done Now You Can Login On Our Website</h2>';
+     }
+    ?>
+
+    <div class="container1">
+        <ul class="navbar">
+            <div class="icon">
+                <a href="index.html"><i class='bx bxs-home'></i></a>
+            </div>
+            <div class="">
+                <a href=""></a>
+            </div>
+            <div class="">
+                <a href=""></a>
+            </div>
+            <div class="">
+                <a href=""></a>
+            </div>
+        </ul>
+    </div>
+        <div class="box">
+            <div class="container">
+            <form action="signup1.php" method="post">
+
+                <h1>Sign Up</h1>
+                <div class="name">
+
+                    <input type="text" name="name" placeholder="Create User Name" required><i class='bx bx-user'></i>
+                </div>
+                <div class="email">
+
+                    <input type="email" name="email" placeholder="Enter Your Email Address" required><i class='bx bxs-envelope'></i>
+                </div>
+                <div class="password">
+
+                    <input type="password" name="password" placeholder="Create Password" required>
+                </div>
+                <div class="cpassword">
+
+                    <input type="password" name="cpassword" placeholder="Confirm Password" required><i class='bx bxs-lock-alt'></i>
+                </div>
+                <div class="button">
+                    <button type="submit">Sign Up</button>
+                </div>
+                <div class="account">Already Have an Account ?? <a href="login1.html">Log In</a></div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
