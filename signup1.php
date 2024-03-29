f61a938528c568bc51f66806d05d4b8a9695d81e
Manthan666 <manthanparekh455@gmail.com>
2024-03-29 22:52:57 +0530

    Database modify by manthan

--- signup1.php
+++ signup1.php
@@ -22,25 +22,10 @@ if (isset($_POST['email'])) {
     $confpassword = $_POST['cpassword'];   
 
     if ($password !== $confpassword) {
-        // function alert($message)
-        // {
-        //     echo "<script>
-        //         alert('$message');
-        //         window.location.href= 'signup.html'
-        //         </script>";
-        // }
-        // alert("Failed to register because your password and confirm password not match");
         $showAlert = true;
     } else{
         $query = mysqli_query($link, "SELECT * FROM `registration` WHERE email = '$email'");
         if (mysqli_num_rows($query) > 0) {
-        //     function alert($message)
-        //     {
-        //         echo "<script>alert('$message');
-        //     window.location.href='login.html'
-        //     </script>";
-        // }
-        // alert("Email id already in use sending you at login page!!");
         $showError = true;
     } else {
         $stmt = mysqli_prepare($link, "INSERT INTO `registration` (`name`, `email`, `password`, `conf.password`, `dt`) VALUES (?, ?, ?, ?, current_timestamp())");
@@ -50,18 +35,10 @@ if (isset($_POST['email'])) {
         
         // Execute the prepared statement
         if (mysqli_stmt_execute($stmt)) {
-            // function alert($message)
-            //     {
-            //         echo "<script>alert('$message');
-            //         window.location.href='index.html'
-            //         </script>";
-            //     }
-            //     alert("You are registered successfully , Thank you for registration");
             $showSuccess = true;
             } else {
                 echo "Facing some technical issue sorry for this inconvinience caused!!";
             }
-            
             // Close the prepared statement
             mysqli_stmt_close($stmt);
         }
